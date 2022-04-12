8676346a27679b6e2b4ab7128843a25f59da7b49
shyamlal <shyamlal@qburst.com>
2022-04-12 23:46:47 +0530

    Custom Navigation Bar.

--- gecwapp/lib/screens/homeScreen.dart
+++ gecwapp/lib/screens/homeScreen.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:gecwapp/CustomWidgets/customAppbar.dart';
+import 'package:gecwapp/CustomWidgets/navdrawer.dart';
 import 'package:gecwapp/CustomWidgets/tabbarMenu.dart';
 import 'package:gecwapp/Screens/busTimings.dart';
 import 'package:gecwapp/Screens/hostelListScreen.dart';
 
 class HomeScreen extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
         length: 3,
         child: Scaffold(
+          drawer: NavDrawer(),
+          // appBar: CustomAppBar(),
           bottomNavigationBar: TabBarMenu(),
           body: TabBarView(children: [
             HostelList(),
