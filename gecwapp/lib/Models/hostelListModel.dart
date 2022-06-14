c4feb1aec43e6b1d609d5c15e976e9ccce5777cf
shyam-lal <slal4252@gmail.com>
2022-06-14 17:42:35 +0530

    New hostel model class

--- gecwapp/lib/Models/hostelListModel.dart
+++ gecwapp/lib/Models/hostelListModel.dart
@@ -23,3 +23,29 @@ class HostelListModel {
   Map<dynamic, dynamic> toJson() =>
       <dynamic, dynamic>{'id': id, 'name': name, 'image': image};
 }
+
+
+// class HostelListModel {
+//   // final String id;
+//   final String name, location, phoneNum, warden;
+//   final bool hasFood;
+//   final List<String> images, places;
+
+//   HostelListModel(this.name, this.location, this.phoneNum, this.warden,
+//       this.hasFood, this.images, this.places);
+
+//   HostelListModel.fromJson(Map<dynamic, dynamic> json)
+//       // : date = DateTime.parse(json['date'] as String),
+//         : name = json['name'] as String,
+//         location = json['location'] as String,
+//         phoneNum = json['phone'] as String,
+//         warden = json['warden'] as String,
+//         hasFood = json['food'] as bool,
+//         images = json['images'] as List<String>,
+//         places = json['near'] as List<String>;
+
+
+
+//   // Map<dynamic, dynamic> toJson() =>
+//   //     <dynamic, dynamic>{'id': id, 'name': name, 'image': image};
+// }
