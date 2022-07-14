5c3b10f3c7adcce78202813844bc46fe7f556d0e
shyam-lal <slal4252@gmail.com>
2022-07-15 00:35:42 +0530

    Hostel list search logic.

--- gecwapp/lib/screens/hostelListScreen.dart
+++ gecwapp/lib/screens/hostelListScreen.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:gecwapp/Models/hostelListModel.dart';
 import 'package:gecwapp/Providers/hostels_provider.dart';
 import 'package:gecwapp/Providers/sharedPrefs_provider.dart';
 import 'package:gecwapp/customWidgets/hostelListItem.dart';
 import 'package:provider/provider.dart';
 
-class HostelListScreen extends StatelessWidget {
-  var hostelData = [];
-  // var userAccess;
+class HostelListScreen extends StatefulWidget {
+  @override
+  State<HostelListScreen> createState() => _HostelListScreenState();
+}
 
-  // @override
-  // void initState() {
-  //   // TODO: implement initState
-  //   super.initState();
-  //   getHostelList();
-  //   getSharedPrefs();
-  // }
+class _HostelListScreenState extends State<HostelListScreen> {
+  List<HostelListModel> hostelData = [];
+  int userAccess = 0;
+  List<HostelListModel> _foundItems = [];
+
+  // This list holds the data for the list view
+  @override
+  initState() {
+    // hostelData = context.watch<HostelProvider>().hostels;
+    // userAccess = context.watch<SharedPrefsProvider>().userAccess;
+    // context.read<HostelProvider>().fetchUserDetails();
+
+    // _foundItems = dataset;
+    // super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // context.read<UserProvider>().fetchUserDetails;
-    // context.read<NotificationProvider>().getNotifications;
     hostelData = context.watch<HostelProvider>().hostels;
     int userAccess = context.watch<SharedPrefsProvider>().userAccess;
     if (hostelData.isEmpty) {
       context.read<HostelProvider>().fetchUserDetails();
     }
 
-    // print(userAccess);
     return Scaffold(
         body: hostelData.isEmpty
             ? Center(child: CircularProgressIndicator())
@@ -54,6 +61,30 @@ class HostelListScreen extends StatelessWidget {
                         ],
                       ),
                     ),
+                    //////////
+                    ////////
+                    ///////////Search bar
+
+                    Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 10, 20, 15),
+                        child: TextField(
+                          onChanged: (value) => _runFilter(value),
+                          textAlign: TextAlign.left,
+                          // controller: _controller,
+                          decoration: InputDecoration(
+                              suffixIcon: Icon(Icons.search),
+                              hintText: "Search",
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color:
+                                          Color.fromARGB(255, 189, 189, 189)))),
+                        )),
+                    /////
+                    ////////
+                    ///List Body
+
                     Expanded(
                       child: RefreshIndicator(
                         onRefresh:
@@ -64,7 +95,7 @@ class HostelListScreen extends StatelessWidget {
                           separatorBuilder: (BuildContext context, int index) {
                             return SizedBox(height: 20);
                           },
-                          itemCount: hostelData.length,
+                          itemCount: _foundItems.length,
                           itemBuilder: (BuildContext context, int index) {
                             return HostelListItem(index);
                           },
@@ -76,9 +107,124 @@ class HostelListScreen extends StatelessWidget {
               ));
   }
 
+  // This function is called whenever the text field changes
+  void _runFilter(String enteredKeyword) {
+    List<HostelListModel> results = [];
+    if (enteredKeyword.isEmpty) {
+      // if the search field is empty or only contains white-space, we'll display all users
+      results = hostelData;
+    } else {
+      results = hostelData
+          .where((item) =>
+              item.name.toLowerCase().contains(enteredKeyword.toLowerCase()))
+          .toList();
+      // we use the toLowerCase() method to make it case-insensitive
+    }
+
+    // Refresh the UI
+    setState(() {
+      _foundItems = results;
+    });
+  }
+
   editHostel(BuildContext context) {
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       content: Text("Swantham aayi cheyy myre"),
     ));
   }
 }
+
+
+
+
+
+// class HostelListScreen extends StatelessWidget {
+//   var hostelData = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     hostelData = context.watch<HostelProvider>().hostels;
+//     int userAccess = context.watch<SharedPrefsProvider>().userAccess;
+//     if (hostelData.isEmpty) {
+//       context.read<HostelProvider>().fetchUserDetails();
+//     }
+
+//     return Scaffold(
+//         body: hostelData.isEmpty
+//             ? Center(child: CircularProgressIndicator())
+//             : SafeArea(
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Padding(
+//                       padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+//                       child: Row(
+//                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                         children: [
+//                           Text(
+//                             "Hostels",
+//                             style: TextStyle(
+//                                 fontSize: 25, fontWeight: FontWeight.bold),
+//                           ),
+//                           userAccess == 2
+//                               ? IconButton(
+//                                   onPressed: () {
+//                                     editHostel(context);
+//                                   },
+//                                   icon: Icon(Icons.edit))
+//                               : SizedBox()
+//                         ],
+//                       ),
+//                     ),
+//                     //////////
+//                     ////////
+//                     ///////////Search bar
+
+//                     Padding(
+//                         padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+//                         child: TextField(
+//                           // onChanged: (value) => _runFilter(value),
+//                           textAlign: TextAlign.left,
+//                           // controller: _controller,
+//                           decoration: InputDecoration(
+//                               suffixIcon: Icon(Icons.search),
+//                               hintText: "Search",
+//                               enabledBorder: OutlineInputBorder(
+//                                   borderRadius: BorderRadius.circular(25.0),
+//                                   borderSide: BorderSide(
+//                                       width: 1,
+//                                       color:
+//                                           Color.fromARGB(255, 189, 189, 189)))),
+//                         )),
+//                     /////
+//                     ////////
+//                     ///List Body
+
+//                     Expanded(
+//                       child: RefreshIndicator(
+//                         onRefresh:
+//                             context.read<HostelProvider>().fetchUserDetails,
+//                         child: ListView.separated(
+//                           shrinkWrap: true,
+//                           // physics: ClampingScrollPhysics(),
+//                           separatorBuilder: (BuildContext context, int index) {
+//                             return SizedBox(height: 20);
+//                           },
+//                           itemCount: hostelData.length,
+//                           itemBuilder: (BuildContext context, int index) {
+//                             return HostelListItem(index);
+//                           },
+//                         ),
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ));
+//   }
+
+//   editHostel(BuildContext context) {
+//     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+//       content: Text("Swantham aayi cheyy myre"),
+//     ));
+//   }
+// }
