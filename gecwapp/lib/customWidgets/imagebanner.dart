56778e65a184204b87614a4b875398c5bd331236
shyam-lal <slal4252@gmail.com>
2022-07-28 16:29:45 +0530

    Add image cache dependency.

--- gecwapp/lib/customWidgets/imagebanner.dart
+++ gecwapp/lib/customWidgets/imagebanner.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class ImageBanner extends StatelessWidget {
@@ -12,8 +13,14 @@ class ImageBanner extends StatelessWidget {
       // margin: EdgeInsets.only(right: 5.0),
       child: ClipRRect(
         borderRadius: BorderRadius.all(Radius.circular(20)),
-        child: Image.network(
-          _assetPath,
+        // child: Image.network(
+        //   _assetPath,
+        //   width: imageWidth,
+        //   height: imageHeight,
+        //   fit: BoxFit.fill,
+        // )
+        child: CachedNetworkImage(
+          imageUrl: _assetPath,
           width: imageWidth,
           height: imageHeight,
           fit: BoxFit.fill,
