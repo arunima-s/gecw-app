3323acf51fbb45948a07835016a33eaa49a18c7f
shyam-lal <slal4252@gmail.com>
2022-08-01 10:24:17 +0530

    UI changes in notification details screen.

--- gecwapp/lib/screens/NotificationScreens/notification-detail-screen.dart
+++ gecwapp/lib/screens/NotificationScreens/notification-detail-screen.dart
@@ -6,6 +6,7 @@ import 'package:gecwapp/Providers/gw_values_provider.dart';
 import 'package:gecwapp/Providers/notification_provider.dart';
 import 'package:gecwapp/customWidgets/imagebanner.dart';
 import 'package:gecwapp/customWidgets/rounded_button.dart';
+import 'package:gecwapp/customWidgets/simple_widgets.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -26,104 +27,114 @@ class NotificationDetailsScreen extends StatelessWidget {
       body: SafeArea(
         child: Container(
           width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Stack(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ImageBanner(
-                    notificationItem!.image, screenHeight * 0.35, screenWidth),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(15, 15, 15, 10),
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ImageBanner(notificationItem!.image,
+                        screenHeight * 0.35, screenWidth),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(15, 15, 15, 10),
+                    width: double.infinity,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            AutoSizeText(
-                              notificationItem!.user,
-                              style: TextStyle(
-                                  fontFamily: 'Nunito',
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w900),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                AutoSizeText(
+                                  notificationItem!.user,
+                                  style: TextStyle(
+                                      fontFamily: 'Nunito',
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w900),
+                                ),
+                                Text(
+                                  "Event Date: ${notificationItem!.eventDate}",
+                                  style: TextStyle(
+                                      fontFamily: 'Nunito',
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500),
+                                )
+                              ],
                             ),
-                            Text(
-                              "Event Date: ${notificationItem!.eventDate}",
-                              style: TextStyle(
-                                  fontFamily: 'Nunito',
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500),
-                            )
+                            IconButton(
+                                onPressed: null,
+                                icon: Icon(Icons.notifications_active_outlined))
                           ],
                         ),
-                        IconButton(
-                            onPressed: null,
-                            icon: Icon(Icons.notifications_active_outlined))
-                      ],
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 15, bottom: 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Details:",
-                              style: TextStyle(
-                                  fontFamily: 'Nunito',
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500)),
-                          SizedBox(
-                            height: screenHeight * 0.02,
+                        Container(
+                          padding: EdgeInsets.only(top: 15, bottom: 15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Details:",
+                                  style: TextStyle(
+                                      fontFamily: 'Nunito',
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500)),
+                              SizedBox(
+                                height: screenHeight * 0.02,
+                              ),
+                              Text(notificationItem!.details,
+                                  style: TextStyle(
+                                      fontFamily: 'Nunito',
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500))
+                            ],
                           ),
-                          Text(notificationItem!.details,
-                              style: TextStyle(
-                                  fontFamily: 'Nunito',
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w500))
-                        ],
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        SizedBox(
-                          height: screenHeight * 0.07,
-                          width: screenWidth * 0.45,
-                          child: ElevatedButton(
-                              style: ButtonStyle(
-                                  foregroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          Colors.red),
-                                  backgroundColor:
-                                      MaterialStateProperty.all<Color>(
-                                          AppColors.primaryColor),
-                                  shape: MaterialStateProperty.all<
-                                      RoundedRectangleBorder>(
-                                    RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(12.0),
-                                        side: BorderSide(
-                                            width: 2,
-                                            color: AppColors.primaryColor)),
-                                  )),
-                              onPressed: () {
-                                openLink(notificationItem!.link);
-                              },
-                              child: Text(
-                                "Register/View Event",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 16),
-                              )),
                         ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            SizedBox(
+                              height: screenHeight * 0.07,
+                              width: screenWidth * 0.45,
+                              child: ElevatedButton(
+                                  style: ButtonStyle(
+                                      foregroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                              Colors.red),
+                                      backgroundColor:
+                                          MaterialStateProperty.all<Color>(
+                                              AppColors.primaryColor),
+                                      shape: MaterialStateProperty.all<
+                                          RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(12.0),
+                                            side: BorderSide(
+                                                width: 2,
+                                                color: AppColors.primaryColor)),
+                                      )),
+                                  onPressed: () {
+                                    openLink(notificationItem!.link);
+                                  },
+                                  child: Text(
+                                    "Register/View Event",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 16),
+                                  )),
+                            ),
+                          ],
+                        )
                       ],
-                    )
-                  ],
-                ),
+                    ),
+                  )
+                ],
+              ),
+              Positioned(
+                top: screenHeight * 0.02,
+                left: screenWidth * 0.02,
+                child: GWRoundedIcon(
+                    Navigator.of(context).pop, Icon(Icons.arrow_back_ios_new)),
               )
             ],
           ),
