9790ffd911b14291709ec5537a902c0367bff076
shyam-lal <slal4252@gmail.com>
2022-08-02 01:30:01 +0530

    Alert for showing CGPA.

--- gecwapp/lib/screens/Tools/cgpa-calculator.dart
+++ gecwapp/lib/screens/Tools/cgpa-calculator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gecwapp/Providers/gw_values_provider.dart';
 import 'package:gecwapp/Utilities/popup_messages.dart';
+import 'package:gecwapp/customWidgets/Alerts/simple-alert.dart';
 import 'package:gecwapp/customWidgets/rounded_button.dart';
 import 'package:provider/provider.dart';
 
@@ -20,10 +21,39 @@ class CGPACalculator extends StatelessWidget {
           shrinkWrap: true,
           physics: ClampingScrollPhysics(),
           children: [
+            SizedBox(height: screenHeight * 0.02),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [Text("Semster"), Text("SGPA")],
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(Icons.arrow_back_ios_new)),
+                Text(
+                  "CGPA Calculator",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                )
+              ],
             ),
+            SizedBox(height: screenHeight * 0.03),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  "Semester",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 15),
+                  child: Text(
+                    "SGPA",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: screenHeight * 0.03),
             ////
             ListView.separated(
               shrinkWrap: true,
@@ -45,6 +75,8 @@ class CGPACalculator extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.only(left: 20, right: 20, top: 25),
               child: RoundedButton("Calculate CGPA", () {
+                sum = 0;
+
                 print(sgpaList);
                 print(sgpaList.length);
                 if (sgpaList.length != 8) {
@@ -53,8 +85,14 @@ class CGPACalculator extends StatelessWidget {
                   sgpaList.values.map((e) {
                     sum = sum + e;
                   }).toList();
-                  Messages.displayMessage(context, "${sum * 0.125}");
-                  sum = 0;
+                  // Messages.displayMessage(context, "${sum * 0.125}");
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext buildContext) {
+                        final average = sum * 0.125;
+                        return SimpleAlert(
+                            "Your CGPA is ${average.toString()}");
+                      });
                 }
               }, screenWidth * 0.5, screenHeight * 0.08),
             )
@@ -82,6 +120,8 @@ class CGPAItem extends StatelessWidget {
           width: 100,
           height: 40,
           child: TextField(
+            textInputAction: TextInputAction.next,
+
             controller: _sgpaController,
             onChanged: (value) {
               if (value == "") {
